9dacddea719f18c91252c0cec4a55be729302bca
Lucas Pollyceno <lucaspollyceno@gmail.com>
2021-03-22 14:50:18 -0300

    Eliminação de desigualdades redundantes de um dado conjunto de desigualdades.

--- DescricaoEntropica/src/redundancy_reduction.py
+++ DescricaoEntropica/src/redundancy_reduction.py
@@ -1,28 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 10 13:52:06 2018
 
-@author: sadra
-"""
 import numpy as np
 import gurobipy as gp
-#import cvxpy as cp
-import time
+#mport time
 
 
 
 def elimina_redundancia(A, B):
     print('Inicial:', len(A))
+
+    # RETIRA DESIGUALDADES REPETIDAS
     A = np.unique(A, axis = 0)
     B = np.zeros((len(A)))
     #B = np.reshape(B, (len(B))) #Transpondo para o gurobi
 
-    print('Após eliminar red triviais:', len(A))
+    #print('Após eliminar red triviais:', len(A))
     cont=0
     c=0
     n=len(A)
-    matriz_elemental = np.zeros((len(A[0])))
     l=0
     print('Aplicando o lema', len(A))
     row=0
@@ -31,44 +25,44 @@ def elimina_redundancia(A, B):
             por = int((cont/n)*100)
             cont=cont+1
             #print('Porcentagem:', por, '%')
+
+        # SEPARANDO DESIGUALDADE A SER TESTADA
         a_row = A[row][:]
         b_row = B[row]
         A_row = np.delete(A, (row), axis = 0)
         B_row = np.delete(B, (row), axis = 0)
 
-        #t1 = time.time()
+        # MONTANDO MODELO
         prob = gp.Model()
         h = prob.addMVar(len(A[0]), lb=-gp.GRB.INFINITY,ub=gp.GRB.INFINITY )
         prob.setObjective(a_row @ h, gp.GRB.MAXIMIZE)
         prob.addConstr(A_row @ h <= B_row)
         prob.update()
         prob.setParam('OutputFlag',False)
-        '''
-        if(cont == 1):
-            print("Modelo----%s segundos----" %(time.time()-t1))
-        t2 = time.time()
-        '''
+
         prob.optimize()
-        '''
-        if(cont == 1):
-            print("Optimize----%s segundos----" %(time.time()-t2))
-        '''
+
         atol=10**-8
-        if((prob.Status == 2)):
+        # E FACTIVEL?
+        if((prob.Status == 2)): 
             #print(prob.ObjVal)
+            # Se cumpre o lema, ELIMINA
             if(prob.ObjVal<atol):
                 A = A_row
                 B = B_row
+            # Se NAO cumpre o lema, NAO elimina
             else:
                 row = row + 1
-        else:
+        #Se NAO e factivel, NAO cumpre o lema, NAO elimina
+        else: 
             #print(prob.ObjVal)
             row = row + 1  
         c=c+1
-    print('Apos reducao:', len(A))
+    #print('Apos reducao:', len(A))
     #Normalizando
-    B = np.reshape(B, (np.size(B),1)) #Transpondo o vetor para poder entrar na funcaoourier Motzkin
+    B = np.reshape(B, (np.size(B),1)) #Transpondo o vetor para poder entrar na funcao Fourier Motzkin
     
+    #RENORMALIZANDO
     H_max=np.amax(abs(A),axis=1)
     A=np.dot(np.diag(1/H_max),A)
     B=np.dot(np.diag(1/H_max),B)
