81ee85f03c23ac1ebcfcac82afd7683cafd8a9da
Lucas Pollyceno <lucaspollyceno@gmail.com>
2021-03-23 19:15:22 -0300

    Fronteira delimitada pela nova desigualdade obtida utilizando o procedimento de redução de desigualdades.

--- fronteira/bipartido/protocolos/novaic.py
+++ fronteira/bipartido/protocolos/novaic.py
@@ -4,7 +4,6 @@ import numpy as np
 from marginalizacao import *
 
 
-
 #------------FUNÇÃO QUE CALCULA A ENTROPIA------------------------------------------
 def H(prob):
 	S=np.size(prob)# Numero de elementos
